40326a72effeace530dc7d48ee2f8a204643b6e0
yongbin3525 <bin352500@naver.com>
2021-08-29 13:34:59 +0900

    add: 1550

---
+++ src/boj/_1500/_1550.java
@@ -0,0 +1,12 @@
+package boj._1500;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _1550 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(Integer.parseInt(bf.readLine(), 16));
+    }
+}
