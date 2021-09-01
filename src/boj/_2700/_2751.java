8b3e3eaa7d423c5fbc1b85e1465c80bee96c38c3
yongbin3525 <bin352500@naver.com>
2021-09-01 22:53:33 +0900

    add: 2751

---
+++ src/boj/_2700/_2751.java
@@ -0,0 +1,25 @@
+package boj._2700;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class _2751 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = 0; i < N; i++) {
+            list.add(Integer.parseInt(br.readLine()));
+        }
+        Collections.sort(list);
+        for(int value : list) {
+            sb.append(value).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
