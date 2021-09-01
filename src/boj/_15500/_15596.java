33e25d8bb030a2ec1dd27073739d86c8d8052e61
Yongbin Lee <yblee@uracle.co.kr>
2021-09-01 12:39:16 +0900

    add: 15596

---
+++ src/boj/_15500/_15596.java
@@ -0,0 +1,11 @@
+package boj._15500;
+
+public class _15596 {
+    long sum(int[] a) {
+        long sum = 0;
+        for (int j : a) {
+            sum += j;
+        }
+        return sum;
+    }
+}
