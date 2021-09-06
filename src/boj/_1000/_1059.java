7052e79fcf34fc7058b57f8ceb3fb79ea5d1edf9
Yongbin Lee <yblee@uracle.co.kr>
2021-09-06 12:27:54 +0900

    add: 1059

---
+++ src/boj/_1000/_1059.java
@@ -0,0 +1,31 @@
+package boj._1000;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _1059 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int cnt = 0;
+        int N = Integer.parseInt(br.readLine());
+        if (N < 100) {
+            cnt = N;
+        } else {
+            cnt = 99;
+            if (N == 1000) {
+                N = 999;
+            }
+
+            for (int i = 100; i <= N; i++) {
+                int hun = i / 100;
+                int ten = (i / 10) % 10;
+                int one = i % 10;
+                if((hun-ten) == (ten-one)){
+                    cnt++;
+                }
+            }
+        }
+        System.out.println(cnt);
+    }
+}
