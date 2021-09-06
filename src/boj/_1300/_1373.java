17bb2661f90fd76f0641d0bc2e645888bed52000
yongbin3525 <bin352500@naver.com>
2021-09-06 23:10:33 +0900

    add: 1373

---
+++ src/boj/_1300/_1373.java
@@ -0,0 +1,9 @@
+package boj._1300;
+
+import java.math.BigInteger;
+
+public class _1373 {
+    public static void main(String[] args) {
+        System.out.println(new BigInteger(new java.util.Scanner(System.in).next(), 2).toString(8));
+    }
+}
