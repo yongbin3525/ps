004cc9fb34453cd17976b320f3fb67e00090e0ea
yongbin3525 <bin352500@naver.com>
2021-09-12 20:06:19 +0900

    add: 2914

---
+++ src/boj/_2900/_2914.java
@@ -0,0 +1,17 @@
+package boj._2900;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _2914 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int I = Integer.parseInt(st.nextToken());
+        System.out.println(A*(I-1)+1);
+
+    }
+}
