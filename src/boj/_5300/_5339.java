1151dabb85bf33b1caed7db2a29060d6f1d79dfb
Yongbin Lee <yblee@uracle.co.kr>
2021-09-13 12:25:33 +0900

    add: 5339

---
+++ src/boj/_5300/_5339.java
@@ -0,0 +1,16 @@
+package boj._5300;
+
+public class _5339 {
+    public static void main(String[] args) {
+        System.out.println("     /~\\\n" +
+                "    ( oo|\n" +
+                "    _\\=/_\n" +
+                "   /  _  \\\n" +
+                "  //|/.\\|\\\\\n" +
+                " ||  \\ /  ||\n" +
+                "============\n" +
+                "|          |\n" +
+                "|          |\n" +
+                "|          |");
+    }
+}
