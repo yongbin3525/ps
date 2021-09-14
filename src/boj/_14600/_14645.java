e52f917dda13bd0d79d77161d36c23d053cb70ab
Yongbin Lee <yblee@uracle.co.kr>
2021-09-14 12:38:57 +0900

    add: 14645

---
+++ src/boj/_14600/_14645.java
@@ -0,0 +1,21 @@
+package boj._14600;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _14645 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        for(int i = 0; i < 4; i++) {
+            br.readLine();
+        }
+        System.out.println("비와이");
+    }
+
+
+    // 입력받을 필요x
+    void other(){
+        System.out.println("비와이");
+    }
+}
