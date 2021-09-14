69e11176e8e7ab0007e737ec27288144170f2ba5
Yongbin Lee <yblee@uracle.co.kr>
2021-09-14 12:41:27 +0900

    add: 15894

---
+++ src/boj/_15800/_15894.java
@@ -0,0 +1,13 @@
+package boj._15800;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class _15894 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(new BigInteger(br.readLine()).multiply(BigInteger.valueOf(4)));
+    }
+}
