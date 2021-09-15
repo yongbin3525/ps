75cc7f257b18e354353c72a6cce80db61b6efa7b
Yongbin Lee <yblee@uracle.co.kr>
2021-09-15 12:51:13 +0900

    add: 13277

---
+++ src/boj/_13200/_13277.java
@@ -0,0 +1,17 @@
+package boj._13200;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class _13277 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        BigInteger A = new BigInteger(st.nextToken());
+        BigInteger B = new BigInteger(st.nextToken());
+        System.out.println(A.multiply(B));
+    }
+}
