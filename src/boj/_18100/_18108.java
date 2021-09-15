2000bf4030856e0df9b98b109278388b885a1ef7
Yongbin Lee <yblee@uracle.co.kr>
2021-09-15 12:41:26 +0900

    add: 18108

---
+++ src/boj/_18100/_18108.java
@@ -0,0 +1,14 @@
+package boj._18100;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _18108 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int year = Integer.parseInt(br.readLine());
+
+        System.out.println(year - 543);
+    }
+}
