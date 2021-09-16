7604a913d5d972f8d729aeaf64664ee1c2862904
Yongbin Lee <yblee@uracle.co.kr>
2021-09-16 12:47:21 +0900

    add: 14928 (추가 확인 필요)

---
+++ src/boj/_14900/_14928.java
@@ -0,0 +1,17 @@
+package boj._14900;
+
+import java.util.Scanner;
+
+public class _14928 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        long temp = 0;
+        for (int i = 0; i < input.length(); i++) {
+            temp = (temp * 10 + (input.charAt(i) - '0')) % 20000303;
+        }
+
+        System.out.println(temp);
+    }
+
+}
