0dd4efbc18ac6fb0948b7036313f41c0ef7f0209
yongbin3525 <bin352500@naver.com>
2021-10-05 21:39:50 +0900

    add: 2231

---
+++ src/boj/_2200/_2231.java
@@ -0,0 +1,31 @@
+package boj._2200;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _2231 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int result = 0;
+
+        for(int i = 0; i < N; i++) {
+            int number = i;
+            int sum = 0;
+
+            while(number != 0) {
+                sum += number % 10;
+                number /= 10;
+            }
+
+            if(sum + i == N) {
+                result = i;
+                break;
+            }
+
+        }
+
+        System.out.println(result);
+    }
+}
