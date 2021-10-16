417c4902ad6b752d5d4ad99f022007b494da69ae
yongbin3525 <bin352500@naver.com>
2021-10-16 14:22:26 +0900

    add: 21300

---
+++ src/boj/_21300/_21300.java
@@ -0,0 +1,20 @@
+package boj._21300;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _21300 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        int f = Integer.parseInt(st.nextToken());
+        System.out.println((a + b + c + d + e + f) * 5);
+    }
+}
