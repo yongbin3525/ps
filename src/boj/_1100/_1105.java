2932749a808235db442a96912c056e53758048d9
Yongbin Lee <bin352500@naver.com>
2021-11-15 19:52:30 +0900

    add: 1105

---
+++ src/boj/_1100/_1105.java
@@ -0,0 +1,31 @@
+package boj._1100;
+
+import java.io.*;
+
+public class _1105 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] lr = br.readLine().split(" ");
+
+        char[] l = lr[0].toCharArray();
+        char[] r = lr[1].toCharArray();
+
+        int answer = 0;
+        if (l.length == r.length) {
+            for (int i = 0; i < l.length; i++) {
+                if (l[i] == r[i] && l[i] == '8') {
+                    answer++;
+                } else if (l[i] != r[i]) {
+                    break;
+                }
+            }
+        }
+
+        bw.write(String.valueOf(answer));
+
+        bw.flush();
+        bw.close();
+    }
+}
