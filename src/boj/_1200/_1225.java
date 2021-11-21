3c470eb73084e8022c90ce1e60d8e202fcf296cd
Yongbin Lee <bin352500@naver.com>
2021-11-21 17:10:50 +0900

    add: 1225

---
+++ src/boj/_1200/_1225.java
@@ -0,0 +1,22 @@
+package boj._1200;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _1225 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] num = br.readLine().split(" ");
+        long result = 0;
+
+        for (int i = 0; i < num[0].length(); i++) {
+            for (int j = 0; j < num[1].length(); j++) {
+                result += (long) Integer.parseInt(num[0].charAt(i) + "") * Integer.parseInt(num[1].charAt(j) + "");
+            }
+        }
+
+        System.out.println(result);
+
+    }
+}
