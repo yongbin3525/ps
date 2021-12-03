065787b06f71b1cf976ba56e10a929f02fee59d4
Yongbin Lee <bin352500@naver.com>
2021-12-03 09:06:30 +0900

    add: 1874

---
+++ src/boj/_1800/_1874.java
@@ -0,0 +1,44 @@
+package boj._1800;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class _1874 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        Stack<Integer> stack = new Stack<>();
+
+        int N = Integer.parseInt(br.readLine());
+
+        int start = 0;
+
+        while (N-- > 0) {
+
+            int value = Integer.parseInt(br.readLine());
+
+            if (value > start) {
+                for (int i = start + 1; i <= value; i++) {
+                    stack.push(i);
+                    sb.append('+').append('\n');
+                }
+                start = value;
+            } else if (stack.peek() != value) {
+                System.out.println("NO");
+                return;
+            }
+
+            stack.pop();
+            sb.append('-').append('\n');
+
+        }
+
+        System.out.println(sb);
+    }
+
+}
