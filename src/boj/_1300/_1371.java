4aca0fe0a56e42fae038dbd4e1610606b47dad32
Yongbin Lee <bin352500@naver.com>
2021-12-08 12:28:20 +0900

    add: 1371

---
+++ src/boj/_1300/_1371.java
@@ -0,0 +1,33 @@
+package boj._1300;
+
+import java.util.Scanner;
+
+public class _1371 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int[] alphabet = new int[26];
+
+        while (scan.hasNextLine()) {
+            String str = scan.nextLine();
+            for (int i = 0; i < str.length(); i++) {
+                if (str.charAt(i) >= 'a' && str.charAt(i) <= 'z') {
+                    alphabet[str.charAt(i) - 'a']++;
+                }
+            }
+        }
+
+        int max = 0;
+        for (int i = 0; i < 26; i++) {
+            if (max < alphabet[i]) {
+                max = alphabet[i];
+            }
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (max == alphabet[i]) {
+                System.out.print((char) (i + 'a'));
+            }
+        }
+    }
+}
