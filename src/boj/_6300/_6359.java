da2958bc26750ce5176233459a0c022708910831
Yongbin Lee <bin352500@naver.com>
2021-12-18 19:21:34 +0900

    add: 6359 재확인

---
+++ src/boj/_6300/_6359.java
@@ -0,0 +1,30 @@
+package boj._6300;
+
+import java.util.Scanner;
+
+public class _6359 {
+    static int N;
+    static int[] d;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int tc = 1; tc <= t; tc++) {
+            N = sc.nextInt();
+            d = new int[N + 1];//0잠김 1열림
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; i * j <= N; j++) {
+                    if (d[i * j] != 0) {
+                        d[i * j] = 0;
+                    } else {
+                        d[i * j] = 1;
+                    }
+                }
+            }
+            int ans = 0;
+            for (int i = 1; i <= N; i++) {
+                ans += d[i];
+            }
+            System.out.println(ans);
+        }
+    }
+}
