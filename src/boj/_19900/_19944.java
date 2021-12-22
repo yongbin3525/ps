441ff6f58b0f4e7022a6ff628d8582f204390336
Yongbin Lee <bin352500@naver.com>
2021-12-22 11:25:32 +0900

    add: 19944

---
+++ src/boj/_19900/_19944.java
@@ -0,0 +1,25 @@
+package boj._19900;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _19944 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        if (M <= 2) {
+            System.out.println("NEWBIE!");
+        } else if (M <= N) {
+            System.out.println("OLDBIE!");
+        } else {
+            System.out.println("TLE!");
+        }
+
+    }
+
+}
