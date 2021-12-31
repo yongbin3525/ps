91383d15deeb4ee810c6bc2531bc428207ff38ba
Yongbin Lee <bin352500@naver.com>
2021-12-31 09:07:43 +0900

    add: 14924

---
+++ src/boj/_14900/_14924.java
@@ -0,0 +1,13 @@
+package boj._14900;
+
+import java.util.Scanner;
+
+public class _14924 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int s = sc.nextInt(), t = sc.nextInt(), d = sc.nextInt();
+        System.out.println(d / (2 * s) * t);
+    }
+
+}
