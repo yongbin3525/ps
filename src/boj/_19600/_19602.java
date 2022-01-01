825a9cc9d107df15149193f96a3273b2b061902f
Yongbin Lee <bin352500@naver.com>
2022-01-01 15:36:13 +0900

    add: 19602

---
+++ src/boj/_19600/_19602.java
@@ -0,0 +1,16 @@
+package boj._19600;
+
+import java.util.Scanner;
+
+public class _19602 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        if ((1 * sc.nextInt()) + (2 * sc.nextInt()) + (3 * sc.nextInt()) >= 10) {
+            System.out.println("happy");
+        } else {
+            System.out.println("sad");
+        }
+    }
+
+}
