876befa78dd18df50f3c10bec4ec25bc18bc79c4
Yongbin Lee <bin352500@naver.com>
2022-01-04 12:25:29 +0900

    add: 15963

---
+++ src/boj/_15900/_15963.java
@@ -0,0 +1,18 @@
+package boj._15900;
+
+import java.util.Scanner;
+
+public class _15963 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long N = sc.nextLong();
+        long M = sc.nextLong();
+        if (N == M) {
+            System.out.println("1");
+        } else {
+            System.out.println("0");
+        }
+    }
+
+}
