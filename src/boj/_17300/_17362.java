9fb97eb077279ed89c4b9ab9f99f71d46c930e1c
Yongbin Lee <bin352500@naver.com>
2022-01-08 10:56:27 +0900

    add: 17362 수정

--- src/boj/_17300/_17362.java
+++ src/boj/_17300/_17362.java
@@ -1,24 +1,32 @@
 package boj._17300;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Scanner;
 
 public class _17362 {
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-        int count = 1;
-        int change = -1;
-        for (int i = 1; i < n; i++) {
-            if (count <= 1) {
-                change *= -1;
-            }
-            count += change;
-            if (count >= 5) {
-                change *= -1;
-            }
-        }
-        System.out.println(count);
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write(finger(Integer.parseInt(br.readLine())) + "\n");
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+
+    public static int finger(int a){
+        a = a - 1;
+        a = a % 8;
+        if(a == 0) return 1;
+        else if(a == 1 || a == 7) return 2;
+        else if(a == 2 || a == 6) return 3;
+        else if(a == 3 || a == 5) return 4;
+        else return 5;
     }
 
 }
