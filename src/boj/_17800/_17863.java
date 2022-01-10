9e3c21139939b2dd5ac748d63da0cdcfc611c5d6
Yongbin Lee <bin352500@naver.com>
2022-01-10 12:33:59 +0900

    add: 17863

---
+++ src/boj/_17800/_17863.java
@@ -0,0 +1,17 @@
+package boj._17800;
+
+import java.util.Scanner;
+
+public class _17863 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        if (s.startsWith("555")) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+
+}
