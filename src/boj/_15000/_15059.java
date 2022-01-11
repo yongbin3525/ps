c55fa9063bf4985e6283252b71de1fc593d51e59
Yongbin Lee <bin352500@naver.com>
2022-01-11 11:33:17 +0900

    add: 15059

---
+++ src/boj/_15000/_15059.java
@@ -0,0 +1,28 @@
+package boj._15000;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _15059 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int Ca = Integer.parseInt(st.nextToken());
+        int Ba = Integer.parseInt(st.nextToken());
+        int Pa = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int ans = 0;
+        int Cr = Integer.parseInt(st.nextToken());
+        int Br = Integer.parseInt(st.nextToken());
+        int Pr = Integer.parseInt(st.nextToken());
+        ans += Cr < Ca ? 0 : Cr - Ca;
+        ans += Br < Ba ? 0 : Br - Ba;
+        ans += Pr < Pa ? 0 : Pr - Pa;
+
+        System.out.println(ans);
+    }
+
+}
