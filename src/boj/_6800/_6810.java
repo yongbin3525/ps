cf734d8adc69388f652ecf13a7c8eea67f3104ba
Yongbin Lee <bin352500@naver.com>
2022-01-14 10:09:18 +0900

    add: 6810

---
+++ src/boj/_6800/_6810.java
@@ -0,0 +1,15 @@
+package boj._6800;
+
+import java.util.Scanner;
+
+public class _6810 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        System.out.println("The 1-3-sum is " + (91 + a * 1 + b * 3 + c * 1));
+    }
+
+}
