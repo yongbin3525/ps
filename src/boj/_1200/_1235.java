1ae61734076747f6f4b0095c4a846d7c70e46901
Yongbin Lee <bin352500@naver.com>
2022-02-01 13:57:58 +0900

    add: 1235

---
+++ src/boj/_1200/_1235.java
@@ -0,0 +1,33 @@
+package boj._1200;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+public class _1235 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        String[] arr = new String[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = br.readLine();
+        }
+
+        for (int i = 1; i <= arr[0].length(); i++) {
+            Set<String> set = new HashSet<>();
+            for (int j = 0; j < N; j++) {
+                set.add(arr[j].substring(arr[0].length() - i));
+            }
+            if (set.size() == N) {
+                System.out.println(i);
+                return;
+            }
+        }
+
+    }
+
+}
