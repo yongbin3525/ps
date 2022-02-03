9fefef718ed8840f943df006e0ef730e6dbdd53c
Yongbin Lee <bin352500@naver.com>
2022-02-03 12:41:48 +0900

    add: 10995

---
+++ src/boj/_10900/_10995.java
@@ -0,0 +1,24 @@
+package boj._10900;
+
+import java.util.Scanner;
+
+public class _10995 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 2 * n; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+
+}
