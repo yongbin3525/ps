122b64761b6a5d81fc8d5438b7041d117f5630f1
Yongbin Lee <bin352500@naver.com>
2022-02-13 15:29:27 +0900

    add: 1402 fix

--- src/boj/_1400/_1402.java
+++ src/boj/_1400/_1402.java
@@ -1,41 +1,18 @@
 package boj._1400;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class _1402 {
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int tCase = scan.nextInt();
-        for (int i = 0; i < tCase; i++) {
-            int a = scan.nextInt();
-            int b = scan.nextInt();
-            int[] arr = new int[a];
-            int temp = 2;
-            int t = 0;
-            while (a != 1) {
-                if (a % temp == 0) {
-                    arr[t] = temp;
-                    a = a / temp;
-                    t++;
-                } else {
-                    temp++;
-                }
-            }
-            int sum = 0;
-            for (int j = 0; j < arr.length; j++) {
-                if (arr[j] == 0) {
-                    break;
-                }
-                sum += arr[j];
-            }
-            System.out.println(sum);
-            if (sum == b) {
-                System.out.println("yes");
-            } else {
-                System.out.println("no");
-            }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        for(int i = 0; i < N; i++) {
+            br.readLine();
+            System.out.println("yes");
         }
-    }
 
+    }
 }
