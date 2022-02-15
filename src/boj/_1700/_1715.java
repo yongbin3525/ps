737c71fbcad5f4422f7c1c2011284df767f2d264
Yongbin Lee <bin352500@naver.com>
2022-02-15 21:07:21 +0900

    add: 1715

---
+++ src/boj/_1700/_1715.java
@@ -0,0 +1,33 @@
+package boj._1700;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class _1715 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Long> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < N; i++) {
+            pq.add(Long.valueOf(br.readLine()));
+        }
+
+        long num = 0;
+        while (pq.size() > 1) {
+            long a = pq.poll();
+            long b = pq.poll();
+
+            num += a + b;
+            pq.add(a + b);
+        }
+
+        System.out.println(num);
+    }
+
+}
