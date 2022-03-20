453817f92ce547f3022c253d1f675cc29ab24fce
Yongbin Lee <bin352500@naver.com>
2022-03-20 17:54:28 +0900

    fix: 11497

--- src/boj/_11400/_11497.java
+++ src/boj/_11400/_11497.java
@@ -4,49 +4,29 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class _11497 {
 
-    static int t, n;
-    static int arr[];
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        t = Integer.parseInt(br.readLine());
-
-        while (t > 0) {
-            t--;
-            n = Integer.parseInt(br.readLine());
-            int min;
-            arr = new int[n];
-            String[] t = br.readLine().split(" ");
-            for (int i = 0; i < n; i++) {
-                arr[i] = Integer.parseInt(t[i]);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int t = 0; t < T; t++) {
+            int N = sc.nextInt();
+            int max = 0;
+            int[] arr = new int[N];
+            for (int n = 0; n < N; n++) {
+                arr[n] = sc.nextInt();
             }
-
-            int ans[] = new int[n];
-            int left = 0;
-            int right = n - 1;
-
             Arrays.sort(arr);
 
-            for (int i = 0; i < n; i++) {
-                if (i % 2 != 0) {
-                    ans[left] = arr[i];
-                    left += 1;
-                } else {
-                    ans[right] = arr[i];
-                    right -= 1;
+            for (int i = 0; i < N; i++) {
+                if (i + 2 < N) {
+                    max = Math.max(max, arr[i + 2] - arr[i]);
                 }
             }
-            min = Math.abs(ans[0] - ans[n - 1]);
-            for (int i = 1; i < n; i++) {
-                min = Math.max(min, Math.abs(ans[i] - ans[i - 1]));
-            }
-            System.out.println(min);
+            System.out.println(max);
         }
-
     }
 
 }
