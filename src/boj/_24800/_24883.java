b0757ce30f4baa972c1c4e9ed0dd1a57862957c5
Yongbin Lee <bin352500@naver.com>
2022-03-28 14:24:50 +0900

    add: 24883

---
+++ src/boj/_24800/_24883.java
@@ -0,0 +1,17 @@
+package boj._24800;
+
+import java.util.Scanner;
+
+public class _24883 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        if (str.equalsIgnoreCase("N")) {
+            System.out.println("Naver D2");
+        } else {
+            System.out.println("Naver Whale");
+        }
+    }
+
+}
